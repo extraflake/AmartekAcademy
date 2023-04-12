597215660c90f0341a46b8f6b38e823275a1c5f4
Ilham Buono Putra <ilhambuono@gmail.com>
2023-04-12 11:02:17 +0700

    Update Branch

--- Front_End/academi_amartek/src/components/page/user_management/cari_lowongan/index.js
+++
@@ -1,7 +0,0 @@
-import TemplateCariLowongan from "../../../template/user_management/cari_lowongan";
-
-function CariLowongan() {
-	return <TemplateCariLowongan />;
-}
-
-export default CariLowongan;
--- Front_End/academi_amartek/src/components/page/user_management/tentang/index.js
+++
@@ -1,7 +0,0 @@
-import TemplateTentang from "../../../template/user_management/tentang";
-
-function Tentang() {
-	return <TemplateTentang />;
-}
-
-export default Tentang;
